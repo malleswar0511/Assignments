f8c2c0645437ed381d014026d562a70dc6e43298
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-03-12 11:55:43 +0545

    Update bitwise_even.py

--- bitwise_even.py
+++ bitwise_even.py
@@ -5,25 +5,6 @@ Created on Thu Mar 12 10:56:25 2020
 
 @author: mahi
 """
-
-#####   set
-
-myset={"apple","carrot","cherry"}
-for item in myset:
-    print(item)
-
-
-
-set1={1,2,3,4,5,6,7,8,9}
-for item in set1:
-    print(item)
-    
-    
-    
-print("banana" in myset)
-print("carrot" in myset)
-
-
 n = int(input("enter a value : "))
 if n&1:
     print("it is an odd number")
