824385f8cb47c6d592042e4aa43c36cb60b56a57
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 19:14:51 +0545

    Add files via upload

---
+++ program to set nth bit of a number.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 15:39:54 2020
+
+@author: mahi
+"""
+
+def nth_bit(m,n):
+        return ((1<<n|m))
+m=int(input("enter a number :"))
+n=int(input("enter a value toset nth bit :"))
+nth_bit(m,n)
+print("nth bit set number is :",nth_bit(m,n))   
