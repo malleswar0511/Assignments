7dbc5ec2c52c58f0f6a80e1deb6671daf9742930
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 10:53:46 +0545

    Add files via upload

---
+++ Write a program to find maximum between three numbers using conditional operator..py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 15:13:36 2020
+
+@author: mahi
+"""
+
+def maximum_number(x,y,z):
+    if x>y and x>z:
+        print(x,"is grater than",y,z)
+    elif y>z:
+        print(y," is grater than",z,x)
+    else:
+        print(z,"is grater than",y,x )
+x= int(input("enter x value: "))
+y=int(input("enter y value: "))
+z=int(input("enter z value: "))
+number=maximum_number(x,y,z)
+print(number)
