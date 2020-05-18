361cfeac3a2fa6525f0a235578d8ed3c06611543
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 11:10:28 +0545

    Add files via upload

---
+++ Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys.The function should print all key,value combinations..py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 23:10:36 2020
+
+@author: mahi
+"""
+
+def generate_dictionary():
+    dic={}
+    for x in range(1,21):
+        dic[x]=x*x
+    print(dic)
+generate_dictionary() 
+
+
+       
+            
+            
+    
