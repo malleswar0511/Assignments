e4c532743a0216e0964edb984e3b38ac4c2cd4de
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 11:04:03 +0545

    Add files via upload

---
+++ Write a program to swap two numbers using bitwise operator..py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 16:53:10 2020
+
+@author: mahi
+"""
+
+x=int(input("enter first value : "))
+y=int(input("enter second value: "))
+print("Before swaping two numbers : a={0} and b={1}".format(x,y))
+x=x^y
+y=x^y
+x=x^y
+print("After swaping two numbers : a={0} and b={1}".format(x,y))
