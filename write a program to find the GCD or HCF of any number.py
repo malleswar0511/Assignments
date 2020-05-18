52e1513bbd79b1e4acd48b1562c1d68450a55408
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 15:36:46 +0545

    Add files via upload

---
+++ write a program to find the GCD or HCF of any number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 15:07:14 2020
+
+@author: mahi
+"""
+
+def hcf(a,b):
+    if (a==0):
+        return b
+    if (b==0):
+        return a
+    if (a==b):
+        return a
+    if (a>b):
+        return hcf(a-b,b)
+    return hcf(a,b-a)
+a=int(input("enter a value:"))
+b=int(input("enter b value :"))
+if (hcf(a,b)):
+    print("hcf of a and b is ",hcf(a,b))
+else:
+    print("not found")
+    
+    
