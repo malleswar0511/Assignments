ad194dba7acd04a16da7c6db66a5d12cee7882d2
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-18 10:56:16 +0545

    Add files via upload

---
+++ Write a program to check whether year is leap year or not using conditional operator..py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 16:50:33 2020
+
+@author: mahi
+"""
+
+def leap_year(year):
+    if(year%4==0):
+        if(year%100==0):
+            if(year%400==0):
+                print("{0} is a leap year".format(year))
+            else:
+                print("{0} is not a leap year".format(year))
+        else:
+            print("{0} is a leap year ".format(year))
+    else:
+        print("{0} is not a leap year".format(year))
+year=int(input("enter a year:"))
+leap_year(year)
