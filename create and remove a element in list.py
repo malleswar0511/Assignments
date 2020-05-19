4ad50a8276d5d124b436086f27464220a12dc12e
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-19 21:05:06 +0545

    Add files via upload

---
+++ create and remove a element in list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 20:58:58 2020
+
+@author: mahi
+"""
+
+x=int(input("enter a value :"))
+list1=[]
+for i in range(0,x):
+    list1.append(i)
+print(list1)
+list1.pop()
