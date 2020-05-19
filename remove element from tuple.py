b65e1f2cc304253cd0d718abcbaeb5dba1b61cd1
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-19 20:46:33 +0545

    Add files via upload

---
+++ remove element from tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 13:58:01 2020
+
+@author: mahi
+"""
+tup=tuple((1,2,3,4,5,6))
+x=list((tup))
+x.remove(5)
+print(tuple(x))
