c183a365c11677935ca477b0e7cb0ff01745b93d
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-20 11:02:40 +0545

    Add files via upload

---
+++ even or odd condi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 16:54:35 2020
+
+@author: mahi
+"""
+
+def even_odd(number):
+    if number%2==0:
+        print("even")
+    else:
+        print("odd")
+number=int(input("enter a value : "))
+even_odd(number)
