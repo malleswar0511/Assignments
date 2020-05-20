a7fea7555b686725dc888fc1e45e195a034d195f
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-20 07:14:00 +0545

    Add files via upload

---
+++ reversing a list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 07:02:37 2020
+
+@author: mahi
+"""
+#List1=[1,2,3,4,5,6,7,8,9], reverse the list in single line code
+List1=[1,2,3,4,5,6,7,8,9]
+print(List1[::-1])  #method1
+List1.reverse()     #method2
+print(List1)    
