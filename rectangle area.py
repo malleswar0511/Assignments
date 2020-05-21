b1ee7ad3c67eb41b928ad8e2842914b7a8fe6775
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-21 07:02:47 +0545

    Add files via upload

---
+++ rectangle area.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 06:56:49 2020
+
+@author: mahi
+"""
+
+base=float(input("enter base value : "))
+height=float(input("enter height value : "))
+rectangle_area=(base*height)/2
+print("area of rectangle is : ",rectangle_area)
