a0e9b5db1663b77654b4c2531c06dea99f4dc234
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-21 06:53:29 +0545

    Add files via upload

---
+++ equilateral triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 06:46:57 2020
+
+@author: mahi
+"""
+import math
+a=float(input("length of an equilateral triangle : "))
+area=(math.sqrt(3)/4)*(a*a)
+print("area of a equilateral triangle is :",area)
