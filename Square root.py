c7a0e621a8c7c2cf433f7bc1ce352e77efec23b4
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-21 07:07:03 +0545

    Add files via upload

---
+++ Square root.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 07:04:02 2020
+
+@author: mahi
+"""
+import math
+n=int(input("enter a number :"))
+x=math.sqrt(n)
+print(n,"Square root value is",x)
