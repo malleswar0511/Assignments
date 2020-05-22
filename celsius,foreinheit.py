6077f9d44f52a6c60112d37c2fa76b39c6714dba
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-22 06:45:14 +0545

    Add files via upload

---
+++ celsius,foreinheit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 06:39:37 2020
+
+@author: mahi
+"""
+celsius = float(input("enter celsius temparature : "))
+foreinheit=(celsius*1.8)+32
+print("{0} celsius temparature = {1} foreinheit temparature ".format(celsius,foreinheit))
