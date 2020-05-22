e9dc33c0363061b9a5221314b3b038fb5785dc35
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-22 06:54:50 +0545

    Add files via upload

---
+++ length,m,km.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 06:50:24 2020
+
+@author: mahi
+"""
+centi_meter =float(input("enter length in centimeter : "))
+meter = centi_meter/100.0
+kilo_meter=centi_meter/100000.0
+print("length in meters :",meter,"m")
+print("length in kilometers:",kilo_meter,"km")
