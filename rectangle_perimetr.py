e3bf804208592f5cfa3021dedbe1a911a0e331d1
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-22 07:28:55 +0545

    Add files via upload

---
+++ rectangle_perimetr.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 07:21:29 2020
+
+@author: mahi
+"""
+
+height=float(input("enter rectangle height :"))
+width=float(input("enter rectanle width :"))
+perimeter = 2*(height+width)
+print("the perimeter of a rectangle is : ",perimeter)
