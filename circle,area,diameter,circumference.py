5c5829eef66dd724b14d353d51c7aebd30769c31
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-22 07:15:32 +0545

    Add files via upload

---
+++ circle,area,diameter,circumference.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 06:56:23 2020
+
+@author: mahi
+"""
+import math
+radius = float(input("enter radius of a circle :"))
+diameter=2*radius
+circle_area=2*math.pi*radius*radius
+circumference=2*math.pi*radius
+print("diameter :",diameter)
+print("circle_area :",circle_area)
+print("circumference :",circumference)
