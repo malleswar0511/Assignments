22b0293b64f414410b6cad44bdc2521fd37ae7d5
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-22 05:55:34 +0545

    Add files via upload

---
+++ power.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 05:39:36 2020
+
+@author: mahi
+"""
+x=int(input("enter a number :"))
+y=int(input("enter a exponent value :"))
+z= x**y
+print(" {0} power is {1} = {2}".format(x,y,z))
