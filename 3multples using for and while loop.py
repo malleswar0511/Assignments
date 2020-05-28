e2498fb18fd89ea759f1b4ebeefb849576b2236e
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-28 19:44:26 +0545

    Add files via upload

---
+++ 3multples using for and while loop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 19:20:01 2020
+
+@author: mahi
+"""
+#using for loop
+n=3
+for i in range(1,101):
+   print(i*3) 
+
+#using while loop
+x=3
+i=1
+while(i<=100):
+   print(i*x)
+   i=i+1
