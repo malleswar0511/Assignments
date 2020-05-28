489457dba2472b1c2af9084d5a21a620581ed0f0
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-28 20:17:01 +0545

    Add files via upload

---
+++ print dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 20:10:32 2020
+
+@author: mahi
+"""
+def dictionary():
+    d=dict()
+    for x in range(1,4):
+        d[x]=x**2
+    print(d)
+dictionary()
