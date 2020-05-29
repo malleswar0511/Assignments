cd5d78aa6ae8fab979ec12677306bc82472c6303
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-29 14:01:14 +0545

    Add files via upload

---
+++ bitwise op.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 13:07:02 2020
+
+@author: mahi
+"""
+a=int(input("enter a number :"))
+if (a&1==1):
+    print("odd")
+else:
+    print("even")
