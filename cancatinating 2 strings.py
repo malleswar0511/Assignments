e4aaf540511d327d06d23bff9b1ce87a6b7221db
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-31 20:49:38 +0545

    Add files via upload

---
+++ cancatinating 2 strings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 20:44:03 2020
+
+@author: mahi
+"""
+
+def strings(str1,str2):
+    return str1+str2
+str1=str(input("enter string1 :"))
+str2=str(input("enter string2 :"))
+print("after cancatinating :",strings(str1,str2))
