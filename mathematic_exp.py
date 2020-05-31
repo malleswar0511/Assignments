92e3f087ecefb863e69fa3fe14c503a5340884ae
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-31 23:16:11 +0545

    Add files via upload

---
+++ mathematic_exp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 22:44:54 2020
+
+@author: mahi
+"""
+exp1=eval(input("enter a number :"))
+exp2=eval(input("enter a number :"))
+print(exp1+exp2)
