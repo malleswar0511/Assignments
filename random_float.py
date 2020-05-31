3e22c849c9e4567e0369d478cdf4669ce4ff2e35
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-05-31 23:24:19 +0545

    Add files via upload

---
+++ random_float.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 23:18:00 2020
+
+@author: mahi
+"""
+
+
+import random
+print("random float numbers between 10 and 100 is :",random.uniform(10,100))
+
