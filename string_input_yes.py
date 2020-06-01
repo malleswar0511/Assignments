dcd33c9cb03e13dc2c4841e841cff43989b12b9c
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-06-01 16:28:12 +0545

    Add files via upload

---
+++ string_input_yes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 16:24:51 2020
+
+@author: mahi
+"""
+
+st=str(input("enter the correct word :"))
+if (st=="YES" or st=="Yes" or st=="yes"):
+    print("yes")
+else:
+    print("no")
