eb0e5cb513e26a438a54a1a30a125d57e3518da2
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-06-03 16:45:52 +0545

    Add files via upload

---
+++ int to str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  3 16:34:45 2020
+
+@author: mahi
+"""
+def integer(num):
+    string=str(num)
+    print(string)
+    print(type(string))
+num=int(input("enter a number :"))
+integer((num))
