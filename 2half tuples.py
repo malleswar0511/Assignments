ac8d16512f9eac1a77c47273a940620ae1847336
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-06-04 07:42:04 +0545

    Add files via upload

---
+++ 2half tuples.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 07:39:13 2020
+
+@author: mahi
+"""
+
+tuple1=(1,2,3,4,5,6,7,8,9,10)
+print(tuple1[:5])
+print(tuple1[5:])
