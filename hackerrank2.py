f24fdcee0f9008fd0de7b954b6774a487c69f8f8
malleswar0511 <51845667+malleswar0511@users.noreply.github.com>
2020-11-25 12:55:49 +0530

    Add files via upload
    this program from HackerRank day 2 Challenge

---
+++ hackerrank2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 11:56:13 2020
+
+@author: DELL
+"""
+
+i=4
+d=4.0
+s="HackerRank "
+
+x=int(input("enter a vlaue:"))
+y=int(input("enter a value:"))
+string=str(input("enter some string :"))
+
+print(x+i)
+print(y+d)
+print(s+string)
